4bd1f47607c48b94679dac30557ffc44123f5190
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-20 14:27:01 -0800

    Node.java

--- Project 02/Node.java
+++ Project 02/Node.java
@@ -1,1 +1,59 @@
+// Programmer : Jsako
 
+public class Node {
+    
+    char item;
+    Node next;
+    public Node() {
+        
+        setItem(' ');
+        setNext(null);
+        
+    }
+    
+    public Node(char newItem) {
+        
+        setItem(newItem);
+        setNext(null);
+        
+    }
+    
+    public Node(char newItem, Node newNext) {
+        
+        setItem(newItem);
+        setNext(newNext);
+        
+    }
+    
+    public Node(Node newNext) {
+        
+        setItem(' ');
+        setNext(newNext);
+        
+    }
+    
+    public void setItem(char newItem) {
+        
+        item = newItem;
+        
+    }
+    
+    public void setNext(Node newNext) {
+        
+        next = newNext;
+        
+    }
+    
+    public char getItem() {
+        
+        return item;
+        
+    }
+    
+    public Node getNext() {
+        
+        return next;
+        
+    }
+    
+}
