9d003c1be1e42d6650f86381491308ad8b44c5c2
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-21 17:44:56 -0800

    StackException.java

---
+++ Project 03/Phase I/StackException.java
@@ -0,0 +1,11 @@
+// Programmer : Jsako
+
+package phase1;
+
+public class StackException extends java.lang.RuntimeException {
+    
+    public StackException(String s) {
+        super(s);
+    }
+    
+}
