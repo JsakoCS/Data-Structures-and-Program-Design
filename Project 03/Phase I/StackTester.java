5c60c4114c2b6dc0f2ae8ff55ae6261a909bd224
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-21 17:31:31 -0800

    StackTester.java

--- Project 03/Phase I/StackTester.java
+++ Project 03/Phase I/StackTester.java
@@ -3,21 +3,21 @@
 package phase1;
 
 public class StackTester {
-  
-public static final int MAX_ITEMS = 15;
-
-public static void main(String[] args) {
-    StackArrayBased stack = new StackArrayBased();
-    Integer items[] = new Integer[MAX_ITEMS];
-    for (int i=0; i<MAX_ITEMS; i++){
-        items[i] = new Integer(i);
-        if (!stack.isFull()){
-            stack.push(items[i]);
+    
+    public static final int MAX_ITEMS = 15;
+    
+    public static void main(String[] args) {
+        StackArrayBased stack = new StackArrayBased();
+        Integer items[] = new Integer[MAX_ITEMS];
+        for (int i=0; i<MAX_ITEMS; i++){
+            items[i] = new Integer(i);
+            if (!stack.isFull()){
+                stack.push(items[i]);
+            }
+        }
+        while (!stack.isEmpty()){
+            System.out.println((Integer)(stack.pop()));
         }
     }
-    while (!stack.isEmpty()){
-        System.out.println((Integer)(stack.pop()));
-    }
-}
-  
+    
 }
