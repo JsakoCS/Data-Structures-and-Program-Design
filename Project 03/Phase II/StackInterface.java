ca19269e3f7a9142e420a714b834ef2af9675fb3
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-22 21:32:36 -0800

    StackInterface.java

---
+++ Project 03/Phase II/StackInterface.java
@@ -0,0 +1,13 @@
+// Programmer : Jsako
+
+package phase2;
+
+public interface StackInterface {
+    
+    public boolean isEmpty();
+    public void popAll();
+    public void push(Object newItem) throws StackException;
+    public Object pop() throws StackException;
+    public Object peek() throws StackException;
+
+}
