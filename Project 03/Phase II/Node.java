c755abd27debf0c3d372ee8d74a7c8e6f133cf57
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-22 21:26:16 -0800

    Node.java

--- Project 03/Phase II/Node.java
+++ Project 03/Phase II/Node.java
@@ -1,1 +1,40 @@
+// Programmer : Jsako
 
+package phase2;
+
+public class Node {
+    
+    private int item;
+    private Node next;
+    
+    public Node() {
+        item = 0;
+        next = null;
+    }
+    
+    public Node(int newItem) {
+        setItem(newItem);
+    }
+    
+    public Node(int newItem, Node newNext) {
+        setItem(newItem);
+        setNext(newNext);
+    }
+    
+    public int getItem() {
+        return item;
+    }
+    
+    public Node getNext(){
+        return next;
+    }
+    
+    public void setItem(int newItem) {
+        item = newItem;
+    }
+    
+    public void setNext(Node newNext) {
+        next = newNext;
+    }
+    
+}
