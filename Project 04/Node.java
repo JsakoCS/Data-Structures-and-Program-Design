67d4017adfd23c8353ef9acb8f6c0e376fc54d9d
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-24 17:29:28 -0800

    Node.java

--- Project 04/Node.java
+++ Project 04/Node.java
@@ -1,1 +1,29 @@
+// Programmer : Jsako
 
+class Node{
+    
+    int data;
+    Node nextNode;
+  
+    public Node(int data2) {
+        data = data2;
+        nextNode = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+    
+}
