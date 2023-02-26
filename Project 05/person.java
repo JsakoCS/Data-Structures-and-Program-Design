58950dfac6dca206801dc5ff320b2df301b050ad
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-26 15:15:54 -0800

    person.java

---
+++ Project 05/person.java
@@ -0,0 +1,14 @@
+// Programmer : Jsako
+
+public class person {
+
+    String name;
+    String phoneNum;
+
+    public person(String n, String p)
+    {
+        name = n;
+        phoneNum = p;
+    }
+    
+}
