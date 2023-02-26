df761c633b53cb18e458ca017bfef41e90f6420e
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-26 15:34:39 -0800

    userInterface.java

---
+++ Project 05/userInterface.java
@@ -0,0 +1,11 @@
+// Programmer : Jsako
+
+import java.io.FileNotFoundException;
+
+public interface userInterface {
+    public void add();
+    public void delete();
+    public void find();
+    public void change ();
+    public void quit() throws FileNotFoundException ;
+}
